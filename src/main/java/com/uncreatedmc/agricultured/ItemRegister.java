72cfd5c9cfe28211deea9e6e1ee8077b56469a68
nate jacobs <nathanieljacobs141@gmail.com>
2022-02-13 14:25:38 -0600

    added lettuce and strawberrys no png for straberrys

--- src/main/java/com/uncreatedmc/agricultured/ItemRegister.java
+++ src/main/java/com/uncreatedmc/agricultured/ItemRegister.java
@@ -9,15 +9,33 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemRegister
 {
     public static Item ChiliPepper;
+    public static Item GreenPepper;
+    public static Item StrawBerry;
+    public static Item Lettuce;
     public static Item ChiliPepperSeeds;
+    public static Item GreenPepperSeeds;
+    public static Item StrawBerrySeeds;
+    public static Item LettuceSeeds;
 
     public static void RegisterAll(RegistryEvent.Register<Item> itemRegister)
     {
         // ground crops
         ChiliPepper = register("chilipepper", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB).food(FoodData.TINY_VEGETABLE)));
+        GreenPepper = register("greenpepper", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB).food(FoodData.TINY_VEGETABLE)));
+        StrawBerry = register("strawberry", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB).food(FoodData.TINY_VEGETABLE)));
+        Lettuce =register("lettuce", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB).food(FoodData.TINY_VEGETABLE)));
 
         // seeds
         ChiliPepperSeeds = register("chilipepper_seeds", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB)));
+        GreenPepperSeeds = register("greenpepper_seeds", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB)));
+        StrawBerrySeeds = register("strawberry_seeds", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB)));
+        LettuceSeeds = register("lettuce_seeds", new Item((new Item.Properties()).tab(Agricultured.CREATIVE_TAB)));
+
+        //tree fruit
+
+        //saplings
+
+
     }
 
     private static <T extends Item> T register(String name, T item) {
